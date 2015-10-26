fedb8789316cf5ce9d31003b43454717f18972c2
Philip Dow <phil@phildow.net>
2015-10-26 13:39:23 -0500

    added solution for accounts jquery exercise

---
+++ C-solutions/18-dom-jquery/jquery/js/people.js
@@ -0,0 +1,101 @@
+var people = [
+  {
+    firstName: "Philip",
+    lastName: "Dow",
+    age: 32,
+    sex: 0,
+    eyes: "brown",
+    hair: "brown",
+    music: "techno"
+  },
+  {
+    firstName: "Mike",
+    lastName: "D",
+    age: 45,
+    sex: 0,
+    eyes: "blue",
+    hair: "black",
+    music: "country"
+  },
+  {
+    firstName: "Kid",
+    lastName: "Rock",
+    age: 43,
+    sex: 0,
+    eyes: "black",
+    hair: "black",
+    music: "rock"
+  },
+  {
+    firstName: "Maria",
+    lastName: "Lacroix",
+    age: 23,
+    sex: 1,
+    eyes: "blue",
+    hair: "blond",
+    music: "indie"
+  },
+  {
+    firstName: "Joe",
+    lastName: "Billings",
+    age: 73,
+    sex: 0,
+    eyes: "brown",
+    hair: "blond",
+    music: "country"
+  },
+  {
+    firstName: "Marcy",
+    lastName: "Searle",
+    age: 33,
+    sex: 1,
+    eyes: "brown",
+    hair: "black",
+    music: "rock"
+  },
+  {
+    firstName: "Eve",
+    lastName: "Mooring",
+    age: 18,
+    sex: 1,
+    eyes: "blue",
+    hair: "black",
+    music: "techno"
+  },
+  {
+    firstName: "Winston",
+    lastName: "Churchill",
+    age: 91,
+    sex: 0,
+    eyes: "brown",
+    hair: "brown",
+    music: "indie"
+  },
+  {
+    firstName: "Julie",
+    lastName: "Bowles",
+    age: 29,
+    sex: 1,
+    eyes: "green",
+    hair: "black",
+    music: "indie"
+  },
+  {
+    firstName: "Grace",
+    lastName: "Kitching",
+    age: 45,
+    sex: 1,
+    eyes: "brown",
+    hair: "blond",
+    music: "country"
+  },
+  {
+    firstName: "Kyle",
+    lastName: "Bard",
+    age: 38,
+    sex: 1,
+    eyes: "blue",
+    hair: "blond",
+    music: "indie"
+  }
+]
